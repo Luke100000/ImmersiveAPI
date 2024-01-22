d720f3173884ade426647a79a291607d080404ef
Luke100000 <jonas.peche@aon.at>
2024-01-22 22:34:43 +0100

    made hugging async

--- modules/hugging/hugging.py
+++ modules/hugging/hugging.py
@@ -1,4 +1,6 @@
+import asyncio
 import io
+from concurrent.futures import ThreadPoolExecutor
 from typing import List, Any, Optional
 
 from fastapi import FastAPI
@@ -41,20 +43,44 @@ class TTSRequest(BaseModel):
     text: str
 
 
+worker = ThreadPoolExecutor(max_workers=1)
+
+
+def expand_args(func, args, kwargs):
+    return func(*args, **kwargs)
+
+
+async def run(func, *args, **kwargs):
+    return await asyncio.get_event_loop().run_in_executor(
+        worker,
+        expand_args,
+        func,
+        args,
+        kwargs,
+    )
+
+
+# todo all initializers are not snake case
 def initHugging(app: FastAPI):
     @app.post("/v1/text/mistral")
-    def post_text_mistral(params: TextRequest):
-        text = generate_text(params.messages, **params.model_dump(exclude={"messages"}))
+    async def post_text_mistral(params: TextRequest):
+        text = await run(
+            generate_text,
+            params.messages,
+            **params.model_dump(exclude={"messages"}),
+        )
         return Response(text)
 
     @app.post("/v1/image/sdxl-turbo")
-    def post_image_sdxl_turbo(params: ImageRequest):
-        image = generate_image(**params.model_dump())
+    async def post_image_sdxl_turbo(params: ImageRequest):
+        image = await run(generate_image, **params.model_dump())
         buffer = io.BytesIO()
         image.save(buffer, format="PNG")
         return Response(content=buffer.getvalue(), media_type="image/png")
 
     @app.post("/v1/tts/xtts-v2")
-    def post_tts_xtts(params: TTSRequest):
-        wav = generate_speech(**params.model_dump(), speaker_wav="pirate.mp3")
+    async def post_tts_xtts(params: TTSRequest):
+        wav = await run(
+            generate_speech, **params.model_dump(), speaker_wav="pirate.mp3"
+        )
         return Response(content=wav, media_type="audio/wav")
